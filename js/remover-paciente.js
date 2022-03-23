78f54f10d2df638f938a32e8f3fa4c1c73c310a5
Renato Vieira Santos <renato.vieiralhp@gmail.com>
2022-03-23 01:06:16 -0300

    Correção de Bugs

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,5 +3,10 @@ var tabelaAlvo = document.querySelector("#tabela-pacientes");
 tabelaAlvo.addEventListener("dblclick", function(event) {
     var alvoEvento = event.target;
     var paiDoAlvo = alvoEvento.parentNode;
-    paiDoAlvo.remove();
+    paiDoAlvo.classList.add("fadeOut");
+
+    setTimeout(function(){
+        paiDoAlvo.remove()
+    },500);
+
 });
